9daab7943c247efbb830502f41a467adc809272f
DISSANAYAKE D M D C <161795577+it23782518@users.noreply.github.com>
2025-05-19 09:50:38 +0530

    Delete Backend/src directory

--- Backend/src/main/java/com/example/Backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Backend.repository;
-
-import com.example.Backend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
